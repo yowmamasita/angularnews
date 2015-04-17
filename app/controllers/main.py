fc809c6c95dd12f68e7a8b63d8dbe14f34a423bf
Ben Sarmiento <me@bensarmiento.com>
2015-04-18 06:39:44 +0800

    get stories

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -1,12 +1,5 @@
 from ferris import Controller, messages, route_with
 from app.models.story import Story
-import requests
-import time
-
-THREE_MONTHS = 7889220
-SIX_MONTHS = 15778440
-API_URL = "http://hn.algolia.com"
-API_VERSION = "v1"
 
 
 class Main(Controller):
@@ -17,19 +10,4 @@ class Main(Controller):
 
     @route_with('/')
     def api_list(self):
-        timestamp = int(time.time())
-        ret = []
-        tags = ['angular', 'angularjs']
-        for tag in tags:
-            url = API_URL + "/api/" + API_VERSION + "/search?tags=story"
-            url += "&query=%s&numericFilters=points>=50,created_at_i>=" % tag
-            resp = requests.get(url + str(timestamp - THREE_MONTHS))
-            results = resp.json()
-            print results['page']
-            print results['nbHits']
-            print results['nbPages']
-            print results['hitsPerPage']
-            print '-----------------'
-            for hit in results['hits']:
-                ret.append(Story(**{k: v for k, v in hit.iteritems() if k in Story._properties.keys()}))
-        self.context['data'] = ret
+        return 200
---
+++ app/services/hackernews.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+ONE_WEEK = 604800
+API_URL = "http://hn.algolia.com"
+API_VERSION = "v1"
+MINIMUM_KARMA = 50
+HITS_PER_PAGE = 50
+
+
+def get_stories(tags, timestamp=None):
+    if not timestamp:
+        timestamp = int(time.time())
+    # tags = ['angular', 'angularjs']
+    ret = {}
+    while tags:
+        tag = tags.pop()
+        page = 0
+        nbPages = 2
+        while page < nbPages - 1:
+            url = "%s/api/%s/search?tags=story" % (API_URL, API_VERSION,)
+            url += "&query=%s&numericFilters=points>=%d,created_at_i<=%d,created_at_i>%d&page=%d&hitsPerPage=%d" % (tag, MINIMUM_KARMA, timestamp, timestamp - ONE_WEEK, page, HITS_PER_PAGE)
+            resp = requests.get(url)
+            results = resp.json()
+            page = int(results['page'])  # current page
+            nbPages = int(results['nbPages'])  # total pages
+            ret.update({res['title']: res['url'] for res in results['hits']})
+    return ret
+
+print get_stories(['angular', 'angularjs'])
